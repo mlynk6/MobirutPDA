cca40cb582dea4a617d2aabbd7b94ca0fb49ad24
drvc93 <drvc_1993@hotmail.com>
2018-01-17 11:57:19 -0500

    ---

--- app/src/main/java/filtroslys/mobirutpda/ZGConst.java
+++ app/src/main/java/filtroslys/mobirutpda/ZGConst.java
@@ -2,4 +2,11 @@ package filtroslys.mobirutpda;
 
 public class ZGConst {
     public static String ErrRptaServ = "Error Insertando Informacion.";
+    public static  int layout_error = R.drawable.toast_error;
+    public static  int icon_warning = R.drawable.icn_error_24;
+    public static  int layot_warning = R.drawable.toast_warning;
+    public static  int layout_success = R.drawable.toast_success;
+    public static  int icon_succes = R.drawable.ic_done24;
+    public static int icon_error = R.drawable.icn_error_24b;
+
 }
